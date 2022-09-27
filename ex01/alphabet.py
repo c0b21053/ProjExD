bd85a5314854263f586ce858565b7050fda54bbb
C0B21053 <c0b2105366@edu.teu.ac.jp>
2022-09-27 17:56:04 +0900

    while文の採用 #1

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -25,18 +25,20 @@ def kaitou(a,b):
     ans = input("欠損文字はいくつあるでしょうか？")
     if ans == "2":
         print("正解です。それでは、具体的に欠損文字を一つずつ入力してください。")
+        while True:
+            ans1 = input("一つ目の文字を入力してください")
+            if ord(ans1)-64 in a:
+                n = ord(ans1)-64
+                a.remove(n)
+                ans2 = input("二つ目の文字を入力してください")
+                if ord(ans2)-64 in a:
+                    print("正解です！")
+                    break
+                else:
+                    print("不正解です。またチャレンジしてください")
+                    continue
     else:
         print("不正解です。またチャレンジしてください。")
-    ans1 = input("一つ目の文字を入力してください")
-    if ord(ans1)-64 in a:
-        n = ord(ans1)-64
-        a.remove(n)
-        ans2 = input("二つ目の文字を入力してください")
-        if ord(ans2)-64 in a:
-            print("正解です！")
-    else:
-        print("不正解です。またチャレンジしてください")
-        print(ord(ans1),a)
-        
+      
 if __name__ == "__main__":
     syutudai()
