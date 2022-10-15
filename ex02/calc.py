8d09fdd3add875cc377d89846ea516fcfb2cea26
C0B21053 <c0b2105366@edu.teu.ac.jp>
2022-10-15 14:52:24 +0900

    電卓完了

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,6 +9,13 @@ def button_click(event):
     #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
     entry.insert(tk.END,txt)
 
+def cli_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, res)
+
+
 root = tk.Tk()
 root.geometry("300x500")
 
@@ -16,7 +23,7 @@ entry = tk.Entry(root, width=10, font=("",40), justify="right")
 entry.grid(row=0,column=0,columnspan=3)
 
 r,c  = 1, 0
-for i,num in enumerate(range(9,-1,-1),1):
+for i,num in enumerate(list(range(9,-1,-1))+["+",],1):
     button = tk.Button(root,text=num,font=("",30), width=4, height=2)
     button.bind("<1>",button_click)
     button.grid(row=r,column=c)
@@ -26,5 +33,8 @@ for i,num in enumerate(range(9,-1,-1),1):
     else:
         c+=1
 
+btn = tk.Button(root,text=f"=",font=("",30), width=4, height=2)
+btn.bind("<1>",cli_equal)
+btn.grid(row=r,column=c)
 
 root.mainloop()
