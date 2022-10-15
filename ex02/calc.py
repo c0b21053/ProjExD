4ddcafae94a2a80380f7e7da99e2deb1a0088a29
C0B21053 <c0b2105366@edu.teu.ac.jp>
2022-10-15 14:27:38 +0900

    電卓練習4完了

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+from numpy import column_stack
+
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
@@ -9,7 +11,10 @@ def button_click(event):
 root = tk.Tk()
 root.geometry("300x500")
 
-r,c  = 0, 0
+entry = tk.Entry(root, width=10, font=("",40))
+entry.grid(row=0,column=0,columnspan=3)
+
+r,c  = 1, 0
 for i,num in enumerate(range(9,-1,-1),1):
     button = tk.Button(root,text=num,font=("",30), width=4, height=2)
     button.bind("<1>",button_click)
