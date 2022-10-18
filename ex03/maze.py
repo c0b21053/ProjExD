a110b5de5139f84faeeff22eaf653e761e263495
C0B21053 <c0b2105366@edu.teu.ac.jp>
2022-10-18 14:44:20 +0900

    maze_7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,28 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_loop():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -=20
+    if key == "Right":
+        cx += 20
+    canv.coords("tori", cx, cy)
+    root.after(100, main_loop)
+    
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -10,5 +33,11 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file = "fig/3.png")
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori")
+
+    key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
+    main_loop()
 
     root.mainloop()
