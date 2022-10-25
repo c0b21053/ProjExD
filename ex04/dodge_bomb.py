1ce4264ec4c85e7578bfbcb34bede1682b088555
C0B21053 <c0b2105366@edu.teu.ac.jp>
2022-10-25 17:16:46 +0900

    last

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,8 +3,19 @@ import sys
 from random import randint
 
 
-def main():
+def check_bound(obj_rct, scr_rct):
+    
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or obj_rct.right > scr_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
 
+
+
+def main():
+    font = pg.font.Font(None, 200)
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     scrn_rct = scrn_sfc.get_rect()
@@ -16,12 +27,23 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
-    bomb_sfc = pg.Surface((20,20))
-    bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    ftori_img = pg.image.load("fig/8.png")
+    ftori_img = pg.transform.rotozoom(ftori_img, 0, 3.0)
+    ftori_rct = ftori_img.get_rect()
+    ftori_rct.center = 800, 600
+
+    bomb_sfc = pg.image.load("fig/bomb.png")
+    bomb_sfc = pg.transform.rotozoom(bomb_sfc, 0, 0.1)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = randint(0, scrn_rct.width)
     bomb_rct.centery = randint(0, scrn_rct.height)
+
+    drink_sfc = pg.image.load("fig/drink.png")
+    drink_sfc = pg.transform.rotozoom(drink_sfc, 0, 0.1)
+    drink_rct = drink_sfc.get_rect()
+
+    vx, vy = +1, +1
+    ms = 1
     
     clock = pg.time.Clock()
     while True:
@@ -32,18 +54,56 @@ def main():
             if event.type == pg.QUIT:
                 return
         
+        smode = 0
+        if smode == 0:
+            scrn_sfc.blit(drink_sfc, [100, 750])
+        
+        
         key_states = pg.key.get_pressed()
         if key_states[pg.K_UP]:
-            tori_rct.centery -=1
+            tori_rct.centery -= ms
         if key_states[pg.K_DOWN]:
-            tori_rct.centery += 1
+            tori_rct.centery += ms
         if key_states[pg.K_LEFT]:
-            tori_rct.centerx -= 1
+            tori_rct.centerx -= ms
         if key_states[pg.K_RIGHT]:
-            tori_rct.centerx += 1
+            tori_rct.centerx += ms
+        
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1:
+            if key_states[pg.K_LEFT]:
+                tori_rct.centerx += ms
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= ms
+        
+        if tate == -1:
+            if key_states[pg.K_UP]:
+                tori_rct.centery += ms
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= ms
         scrn_sfc.blit(tori_sfc, tori_rct)
 
+        
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+        bomb_rct.move_ip(vx,vy)
         scrn_sfc.blit(bomb_sfc, bomb_rct)
+
+        if tori_rct.colliderect(drink_rct):
+            ms += 2
+            smode = 1
+            scrn_sfc.fill((0, 0, 0))
+
+            
+
+        if tori_rct.colliderect(bomb_rct):
+            vx, vy = 0, 0
+            scrn_sfc.fill((0, 0, 0))
+            text = font.render("Game Over", True, (255, 0, 0))
+            scrn_sfc.blit(text, [400, 300])
+            scrn_sfc.blit(ftori_img, [700,600])
+
 
         pg.display.update()
         clock.tick(1000)
